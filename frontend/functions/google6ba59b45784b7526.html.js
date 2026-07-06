eec05985351de3df9c813facbd548303386eea1c
sharma0527 <chsharmabsk@gmail.com>
2026-07-06 22:11:47 +0530

    Remove functions folder to fix GitHub Pages deployment

--- frontend/functions/google6ba59b45784b7526.html.js
+++
@@ -1,7 +0,0 @@
-export async function onRequest(context) {
-  return new Response("google-site-verification: google6ba59b45784b7526.html", {
-    headers: {
-      "Content-Type": "text/html",
-    },
-  });
-}
